381ce7cb1919de19adc352eb1667764430476c2e
Seungwan98 <sinna3210@gmail.com>
2024-10-03 03:06:51 +0900

    feat: FEAT CI_SCRIPTS

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -18,7 +18,7 @@ CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILENAME"
 # 환경 변수에서 값을 가져와서 *.xconfig 파일에 추가하기
 echo "AlgoliaAppId = $AlgoliaAppId" >> "$CONFIG_FILE_PATH"
 echo "AlgoliaApikey = $AlgoliaApikey" >> "$CONFIG_FILE_PATH"
-#echo "#include \"$PODS_XCCONFIG_PATH\"" >> "$CONFIG_FILE_PATH"
+echo "#include \"$PODS_XCCONFIG_PATH\"" >> "$CONFIG_FILE_PATH"
 
 
 
