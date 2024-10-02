911c30659ffe460998c96044469fa621084d9627
Seungwan98 <sinna3210@gmail.com>
2024-10-03 02:25:58 +0900

    feat: FEAT CI_SCRIPTS

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -13,7 +13,7 @@ CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILENAME"
 # 환경 변수에서 값을 가져와서 *.xconfig 파일에 추가하기
 echo "AlgoliaAppId = $AlgoliaAppId" >> "$CONFIG_FILE_PATH"
 echo "AlgoliaApikey = $AlgoliaApikey" >> "$CONFIG_FILE_PATH"
-echo "#include "Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"" >> "$CONFIG_FILE_PATH"
+echo "#include "/Volumes/workspace/repository/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"" >> "$CONFIG_FILE_PATH"
 
 
 # 생성된 *.xconfig 파일 내용 출력
