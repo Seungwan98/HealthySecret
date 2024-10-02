c7960cb84abc51f9cc8d1855897d447d887c6439
Seungwan98 <sinna3210@gmail.com>
2024-10-03 01:43:08 +0900

    feat: FEAT CI_SCRIPTS

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
 #  ci_post_clone.sh
-#  HealthySecret
-#
-#  Created by 양승완 on 10/1/24.
-#
-# Install CocoaPods using Homebrew.
-brew install cocoapods
+# *.xconfig 파일이 생성될 폴더 경로
+FOLDER_PATH="/Volumes/workspace/repository/"
 
-# Install dependencies you manage with CocoaPods.
-pod install
+# PARTS 배열의 두 번째 요소가 "Scheme Name"에 해당
+IFS='-' read -ra PARTS <<< "$CI_XCODE_SCHEME"
+
+# *.xconfig 파일 이름
+CONFIG_FILENAME="Config.xcconfig"
+
+# *.xconfig 파일의 전체 경로 계산
+CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILENAME"
+
+# 환경 변수에서 값을 가져와서 *.xconfig 파일에 추가하기
+echo "BASE_URL = $AlgoliaAppId" >> "$CONFIG_FILE_PATH"
+echo "APP_NAME = $AlgoliaApikey" >> "$CONFIG_FILE_PATH"
+echo "#include "Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.debug.xcconfig"" >> "$CONFIG_FILE_PATH"
+echo "#include "Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"" >> "$CONFIG_FILE_PATH"
+
+
+# 생성된 *.xconfig 파일 내용 출력
+cat "$CONFIG_FILE_PATH"
+
+echo "Config.xcconfig 파일이 성공적으로 생성되었고, 환경변수 값이 확인되었습니다.
