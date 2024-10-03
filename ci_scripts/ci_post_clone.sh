2c30342906eb162a13c47098eeaed2e44571a7a6
Seungwan98 <sinna3210@gmail.com>
2024-10-03 15:26:14 +0900

    feat: FEAT CI_SCRIPTS

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,7 +3,8 @@
 #  ci_post_clone.sh
 # *.xconfig 파일이 생성될 폴더 경로
 FOLDER_PATH="/Volumes/workspace/repository"
-PODS_XCCONFIG_PATH="/Volumes/workspace/repository/Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"
+PODS_XCCONFIG_RELEASE_PATH="/Volumes/workspace/repository/Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"
+PODS_XCCONFIG_DEBUG_PATH="/Volumes/workspace/repository/Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.debug.xcconfig"
 
 # *.xconfig 파일 이름
 
@@ -18,7 +19,8 @@ CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILENAME"
 # 환경 변수에서 값을 가져와서 *.xconfig 파일에 추가하기
 echo "AlgoliaAppId = $AlgoliaAppId" >> "$CONFIG_FILE_PATH"
 echo "AlgoliaApikey = $AlgoliaApikey" >> "$CONFIG_FILE_PATH"
-echo "#include \"$PODS_XCCONFIG_PATH\"" >> "$CONFIG_FILE_PATH"
+echo "#include \"$PODS_XCCONFIG_RELEASE_PATH\"" >> "$CONFIG_FILE_PATH"
+echo "#include \"$PODS_XCCONFIG_DEBUG_PATH\"" >> "$CONFIG_FILE_PATH"
 
 
 
