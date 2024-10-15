414d641cee8dabfff31ccf3168b0d72e47733bed
Seungwan98 <sinna3210@gmail.com>
2024-10-16 01:40:52 +0900

    feaat CI_8

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,7 +3,7 @@
 #  ci_post_clone.sh
 # *.xconfig 파일이 생성될 폴더 경로
 #FOLDER_PATH="/Volumes/workspace/repository"
-PODS_PATH="/Volumes/workspace/repository/Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"
+#PODS_PATH="/Volumes/workspace/repository/Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"
 
 #CONFIG_FILENAME="ConfigRelease.xcconfig"
 ## Install CocoaPods using Homebrew.
@@ -17,8 +17,8 @@ pod update
 #CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILENAME"
 
 # 환경 변수에서 값을 가져와서 *.xconfig 파일에 추가하기
-echo "AlgoliaAppId = $AlgoliaAppId" >> "$PODS_PATH"
-echo "AlgoliaApikey = $AlgoliaApikey" >> "$PODS_PATH"
+#echo "AlgoliaAppId = $AlgoliaAppId" >> "$PODS_PATH"
+#echo "AlgoliaApikey = $AlgoliaApikey" >> "$PODS_PATH"
 
 
 
