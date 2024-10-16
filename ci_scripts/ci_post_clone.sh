ab29bc623c3b38c73c86100ba0292d5e871d19db
Seungwan98 <sinna3210@gmail.com>
2024-10-17 01:54:28 +0900

    feaat CI_20

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,6 +7,8 @@ echo "환경변수 참조 ConfigRelease.xcconfig file 생성시작"
 # Secrets 경로 지정
 cat <<EOF > "/Volumes/workspace/repository/HealthySecret/ConfigRelease.xcconfig"
 
+echo '#include "Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"' >> "/Volumes/workspace/repository/HealthySecret/ConfigRelease.xcconfig"
+
 
 
 AlgoliaAppId = $(AlgoliaAppId)
@@ -17,4 +19,3 @@ EOF
 
 
 
-echo '#include "Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"' > "/Volumes/workspace/repository/ConfigRelease.xcconfig"
