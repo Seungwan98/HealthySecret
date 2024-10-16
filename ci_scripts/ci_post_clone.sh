e6ba809c619ae0673df5c09f74801a16b5024867
Seungwan98 <sinna3210@gmail.com>
2024-10-17 00:19:22 +0900

    feaat CI_14

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-#  ci_post_clone.sh
-# *.xconfig 파일이 생성될 폴더 경로
 
 brew install cocoapods
 pod install
 
-echo "환경변수 참조 Secrets.xcconfig file 생성시작"
+echo "환경변수 참조 ConfigRelease.xcconfig file 생성시작"
 # Secrets 경로 지정
 cat <<EOF > "/Volumes/workspace/repository/HealthySecret/ConfigRelease.xcconfig"
+
 
 
 AlgoliaAppId = $(AlgoliaAppId)
@@ -18,3 +17,4 @@ EOF
 
 
 
+echo '#include "Pods/Target Support Files/Pods-HealthySecret/Pods-HealthySecret.release.xcconfig"' > "/Volumes/workspace/repository/ConfigRelease.xcconfig"
